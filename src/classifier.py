bd6b9d85a155353f8b8517bf4864b89096ee4bd5
moonian <AL@alices-macbook-pro-2.home>
2023-04-19 01:20:12 +0200

    Updated classifier file and added new file called models to store model and helper functions.

--- src/classifier.py
+++ src/classifier.py
@@ -4,23 +4,11 @@ import torch
 
 import numpy as np
 
-def set_reproducible():
-    # The below is necessary to have reproducible behavior.
-    import random as rn
-    import os
-    os.environ['PYTHONHASHSEED'] = '0'
-    # The below is necessary for starting Numpy generated random numbers
-    # in a well-defined initial state.
-    np.random.seed(17)
-    # The below is necessary for starting core Python generated random numbers
-    # in a well-defined state.
-    rn.seed(12345)
-    # same for pytorch
-    random_seed = 1 # or any of your favorite number 
-    torch.manual_seed(random_seed)
-    torch.cuda.manual_seed(random_seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+import models 
+
+import pandas as pd
+
+from sklearn.metrics import accuracy_score
 
 
 class Classifier:
@@ -28,7 +16,41 @@ class Classifier:
     The Classifier: complete the definition of this class template by providing a constructor (i.e. the
     __init__() function) and the 2 methods train() and predict() below. Please donot change
      """
+    def __init__(self):
+        self.config = {
+          # basic infos
+          "max_epochs": 1,
+          "batch_size": 32,
+          
+          # data preprocessing
+          "input_enrichment": "question_sentence_target",
+          
+          # pre-trained language model (transformer)
+          # "bert-base-cased", "cardiffnlp/twitter-roberta-base-sentiment-latest", "roberta-base"
+          "plm_name": "roberta-base",
+          "plm_freeze": False,
 
+          # classifier (linear layers)
+          "cls_dropout_st":     0,
+          "cls_channels":       [3],
+          "cls_activation":     "ReLU",
+          "cls_dropout_hidden": 0.2,
+          "cls_depth":          2,
+          "cls_width":          384,
+          
+          # optimizer
+          "lr": 5e-5,
+          "wd": 1e-2,
+
+          # scheduler
+          "lr_s": "linear",
+          "warmup": 0,
+          
+          # loss function
+          "criterion": "BCE"
+          }
+        
+        self.final_model_path = "best_model.pt"
 
 
     ############################################# comp
@@ -42,6 +64,120 @@ class Classifier:
          OF MODEL HYPERPARAMETERS
         """
 
+        # Preprocess and Initialize objects for training 
+        (max_epochs, trainloader, devloader,
+        model, optimizer, lr_scheduler,
+        criterion) = models.init_training(self.config, train_filename, dev_filename, device)
+
+        # Helper function for training
+        def train_epoch(dataloader, model, optimizer, lr_scheduler, criterion, device):
+    
+          model.train()
+          losses = []
+          lbls = torch.Tensor([])
+          preds = torch.Tensor([])
+          
+          for batch in dataloader:
+              # get ground truth labels of this batch
+              lbls = torch.cat((lbls, batch["labels"].argmax(dim = 1)))
+
+              # move batch to device
+              batch = {k: v.to(device) for k, v in batch.items()}
+
+              # cardiffnlp model does not have token type ids
+              try:
+                  token_type_ids = batch["token_type_ids"]
+              except KeyError:
+                  token_type_ids = None
+          
+              # zero optimizer gradients
+              optimizer.zero_grad()
+              
+              # forward + backward + optimize
+              outputs = model(batch["input_ids"], batch["attention_mask"], token_type_ids)
+              loss = criterion(outputs, batch['labels'].float())
+              loss.backward()
+              optimizer.step()
+              lr_scheduler.step()
+              
+              # logging
+              losses.append(loss.item())
+              preds = torch.cat((preds, outputs.argmax(dim = 1).cpu()))
+              
+          # compute stats
+          acc = accuracy_score(lbls, preds)
+          loss_mean = np.mean(losses)
+          
+          return acc, loss_mean
+        
+        # Helper function for validation
+        def val_epoch(dataloader, model, criterion, device):
+    
+          model.eval()
+          losses = []
+          lbls = torch.Tensor([])
+          preds = torch.Tensor([])
+          
+          with torch.no_grad():
+              for batch in dataloader:
+                  # get ground truth labels of this batch
+                  lbls = torch.cat((lbls, batch["labels"].argmax(dim = 1)))
+
+                  # move batch to device
+                  batch = {k: v.to(device) for k, v in batch.items()}
+                  
+                  # cardiffnlp model does not have token type ids
+                  try:
+                      token_type_ids = batch["token_type_ids"]
+                  except KeyError:
+                      token_type_ids = None
+
+                  # forward
+                  outputs = model(batch["input_ids"], batch["attention_mask"], token_type_ids)
+                  loss = criterion(outputs, batch['labels'].float())
+                  
+                  # logging
+                  losses.append(loss.item())
+                  preds = torch.cat((preds, outputs.argmax(dim = 1).cpu()))
+
+          # compute stats
+          acc = accuracy_score(lbls, preds)
+          loss_mean = np.mean(losses)
+          
+          return acc, loss_mean
+
+        # train and evaluate the model
+        trn_losses = []
+        dev_losses = []
+        trn_accs = []
+        dev_accs = []
+        max_dev_acc = 0
+
+        for epoch in range(1, max_epochs + 1):
+            
+            ##TRAINING##
+            trn_acc, trn_loss = train_epoch(trainloader, model, optimizer,
+                                            lr_scheduler, criterion, device)
+            
+            ##TESTING##
+            dev_acc, dev_loss = val_epoch(devloader, model, criterion, device)
+            
+            ##REPORT##
+            print(f"Epoch [{epoch}/{max_epochs}] -> Trn Loss: {round(trn_loss, 2)}, Dev Loss: {round(dev_loss, 4)}, \
+                Trn Acc: {round(trn_acc, 2)}, Dev Acc: {round(dev_acc, 4)}")
+            
+            ##LOGGING##
+            trn_losses.append(trn_loss)
+            dev_losses.append(dev_loss)
+            trn_accs.append(trn_acc)
+            dev_accs.append(dev_acc)
+
+            ##SAVE BEST MODEL##
+            if dev_acc > max_dev_acc:
+                max_dev_acc = dev_acc
+                torch.save(model.state_dict(), self.final_model_path)
+        
+        return trn_losses, dev_losses, trn_accs, dev_accs
 
 
     def predict(self, data_filename: str, device: torch.device) -> List[str]:
@@ -51,6 +187,48 @@ class Classifier:
           - DO NOT CHANGE THE SIGNATURE OF THIS METHOD
           - PUT THE MODEL and DATA on the specified device! Do not use another device
         """
+        # Load the final_model
+        model = models.TransformerSentimentClassifier(self.config)
+        model.load_state_dict(torch.load(self.final_model_path))
+        model.eval()
+        lbls = torch.Tensor([])
+        preds = torch.Tensor([])
+        
+        # Process datafile (we have to do this again because we need to be able intake datafile of dev and test)
+        def tokenize_func(hf_dataset):
+          return model.lm_tokenizer(hf_dataset["inputs"], truncation = True)
+        hf_dev = models.get_datasets(self.onfig, data_filename)
+        hf_dev_tok   = hf_dev.map(tokenize_func,   batched = True)
+        hf_dev_tok   = hf_dev_tok.remove_columns(["inputs"])
+        data_collator = models.DataCollatorWithPadding(tokenizer = model.lm_tokenizer, padding = True, return_tensors = "pt")
+        devloader   = models.get_dataloader(hf_dev_tok,   self.config["batch_size"], shuffle = True, data_collator = data_collator)
+
+        with torch.no_grad():
+            for batch in devloader:
+                # get ground truth labels of this batch
+                lbls = torch.cat((lbls, batch["labels"].argmax(dim = 1)))
+
+                # move batch to device
+                batch = {k: v.to(device) for k, v in batch.items()}
+
+                # cardiffnlp model does not have token type ids
+                try:
+                    token_type_ids = batch["token_type_ids"]
+                except KeyError:
+                    token_type_ids = None
+
+                # forward
+                outputs = model(batch["input_ids"], batch["attention_mask"], token_type_ids)
+                
+                # logging
+                preds = torch.cat((preds, outputs.argmax(dim = 1).cpu()))
+
+        # compute stats
+        acc = accuracy_score(lbls, preds)
+
+        pred_labels = ['negative' if l == 0 else 'neutral' if l == 1 else 'positive' for l in preds]
+
+        return pred_labels
 
 
 
